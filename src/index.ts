14c363c0d76f66ca67df849a4ac9bab0598d65cd
daniel <daniels.isonline@gmail.com>
2021-08-06 02:46:43 +0300

    decided on technologies

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+const port = 2000;
+
+const app = express();
+
+app.get('/', (req: Request, res: Response): void => {
+    res.status(200);
+    res.send('hi');
+});
+
+app.listen(2000, ()=> {
+// tslint:disable-next-line: no-console
+console.log(`listening on port: ${port}`);
+});
